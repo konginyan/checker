22b5786e1137db674431ff62a589bb59a8225e88
konginyan <903340839@qq.com>
2017-07-07 21:04:54 +0800

    fix bug of fullmatch

--- src/main/java/com/checker/util/fullMatch.java
+++ src/main/java/com/checker/util/fullMatch.java
@@ -7,9 +7,21 @@ public class fullMatch {
     private static final int min = 10;
     public static List<String> cal(String s1, String s2) {
         List<String> result = new ArrayList<>();
+        s2 = CheckUtil.clearWrap(s2);
         String temp = "";
         int len = 0;
         for (int i = 0; i < s1.length(); i++) {
+            if(s1.charAt(i) == '\r' || s1.charAt(i) == '\n'){
+                if (!temp.equals("")&&CheckUtil.unitMatch(temp, s2)){
+                    if(temp.matches("[A-Za-z\\s\\pP]+")){
+                        len = temp.split("\\s|\\pP").length;
+                    }
+                    else len = temp.length();
+                    if(len>=min)result.add(temp);
+                    temp = "";
+                }
+                continue;
+            }
             if (CheckUtil.unitMatch(temp + s1.charAt(i), s2)) {
                 temp += s1.charAt(i);
             } else {
